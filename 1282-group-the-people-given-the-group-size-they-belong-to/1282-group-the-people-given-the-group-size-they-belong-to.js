a244135e7f6ff019055566bafb195a378ec12735
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2022-06-13 10:27:58 +0600

    Time: 153 ms (31.25%), Space: 45.7 MB (96.35%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.js
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.js
@@ -4,6 +4,7 @@
  */
 var groupThePeople = function(groupSizes) {
     
+    // Here sizes return an object like if groupSizes is [2,2,1,1,1,1,1,1], then we sholud find the object is { 2: 2, 1: 6}
     const sizes = groupSizes.reduce((acc, val) => {
         if(acc[val]) {
             acc[val]++
@@ -14,11 +15,13 @@ var groupThePeople = function(groupSizes) {
     }, {});
     
     let output = Object.keys(sizes).reduce((acc, key) => {
+        //find individual key values array
         let indexs = groupSizes.reduce((acc, element, index) => {
             if(key == element) acc.push(index);
             return acc;
         }, []);
         
+        // break down the array by their key number
         let start = 0;
         let end = parseInt(key);
         while(end <= indexs.length) {
