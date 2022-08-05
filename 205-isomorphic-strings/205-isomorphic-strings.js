c23d0da527508f0a5021853cda7d10cf5587993c
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2022-08-05 21:35:44 +0600

    Time: 112 ms (52.91%), Space: 45.1 MB (26.17%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    
+    if(s.length != t.length) return false;
+    
+    let tset = new Set(t.split(""));
+    let hash = {};
+    
+    for(let i=0; i <= s.length-1; i++) {
+        if(tset.has(t[i])) {
+            hash[s[i]] = t[i];
+            tset.delete(t[i]);
+        } 
+    }
+    
+    let string = "";
+    
+    for(let j=0; j<s.length; j++) {
+        string += hash[s[j]]
+    }
+    
+    if(string == t) {
+        return true
+    } else {
+        return false;
+    }
+    
+};
