9a0cd0c0e6f67de19c7744f4e8904d6c91828729
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2022-08-23 21:18:56 +0600

    Time: 133 ms (71.42%), Space: 44.9 MB (82.14%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    // First Attempt
+//     let hash = {};
+    
+//     for(let i=0; i<s.length; i++) {
+//         hash[s[i]] ?  hash[s[i]]++ :  hash[s[i]] = 1;
+//     }
+    
+//     for(let i=0; i<s.length; i++) {
+//         if(hash[s[i]] === 1) return i;
+//     }
+    
+    for(let i=0; i<s.length; i++) {
+        if(s.indexOf(s[i])=== s.lastIndexOf(s[i])) return i;
+    }
+    return -1;
+};
