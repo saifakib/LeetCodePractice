a0b250e8e63da072e34a469b507939e3b3a2ee33
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2022-09-10 21:50:49 +0600

    Time: 89 ms (78.49%), Space: 43.9 MB (65.97%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    if(Math.abs(x) > 2147483648) return 0;
+    
+    let nums = parseInt(x.toString().split("").reverse().join(""))
+    
+    if(nums > 2147483648) return 0;
+    
+    if(x > 0) return nums;
+    else return -nums;
+};
