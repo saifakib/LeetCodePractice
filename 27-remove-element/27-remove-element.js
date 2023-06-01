1069be02c4fb07bb08db14236c09b257693632b5
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2023-06-01 17:44:43 +0600

    Time: 56 ms (73.99%), Space: 42.1 MB (40.98%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var count = 0;
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] !== val) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    return count;
+};
+
