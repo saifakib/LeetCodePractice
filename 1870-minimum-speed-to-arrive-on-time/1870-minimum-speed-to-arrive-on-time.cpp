c2acbc45745adc777bd3249d964f19fa3342fe5c
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2023-12-09 22:00:48 +0600

    Time: 245 ms (44.38%), Space: 101.8 MB (97.59%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private: 
+    bool isPossible(vector<int>& dist, double hour, int currSpreed) {
+        double elapsed_time = 0.0;
+        for(int i = 0; i < dist.size(); i++){
+            if(i != dist.size() - 1) {
+                elapsed_time += (dist[i] + currSpreed - 1) / currSpreed;
+            }
+            else { 
+                elapsed_time += 1.00 * dist[i] / currSpreed;
+            }
+        };
+        return elapsed_time <= hour;
+    }
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int L = 1, R = INT_MAX / 2;
+        while(L < R) {
+            int M = L + (R - L) / 2;
+            if(!isPossible(dist, hour, M)) L = M + 1;
+            else R = M;
+        }
+        return L == INT_MAX / 2 ? -1 : L;
+    }
+};
