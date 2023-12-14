1e3ee4b84fe9cd1295311400c63b23c0c93c0974
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2023-12-14 21:57:06 +0600

    Time: 219 ms (70.41%), Space: 122.6 MB (51.66%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,16 +1,17 @@
+// TC: O(N)
+// MC: O(N) for the adjacency list.
 class Solution {
 public:
+    vector<vector<int>> adj;
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        adj.clear();
         adj.resize(n);
-        int time = 0;
-        for(int i = 0; i < n; i++) {
-            if(manager[i] == -1) time += informTime[i];
-            else adj[manager[i]].push_back(i);
-        };
-        return time + dfs(headID, informTime);;
+        for (int i = 0; i < n; i++) {
+            if (manager[i] != -1) adj[manager[i]].push_back(i);
+        }
+        return informTime[headID] + dfs(headID, informTime);
     }
 private:
-    vector<vector<int>> adj;
     int dfs(int& headID, vector<int>& informTime) {
         int minutes = 0;
         for(auto id: adj[headID]) {
