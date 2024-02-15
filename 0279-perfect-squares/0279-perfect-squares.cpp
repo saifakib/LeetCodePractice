37f29e3a4be98a6365448fdeb0172db24b2c9a63
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2024-02-15 21:21:57 +0600

    Time: 88 ms (30.12%), Space: 11.2 MB (21.80%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,8 +2,23 @@
 // MC: O(n)
 class Solution {
 public:
-    int numSquares(int n) {
-        // Iterative way
+    vector<int> cache;
+    int solver(int n) {
+        if(n == 0) return 0;
+        if(cache[n] != -1) return cache[n];
+        int res = INT_MAX / 2;
+        for(int i = 1; i*i <= n; i++) {
+            res = min(res, 1 + solver(n - (i*i)));
+        }
+        return cache[n] = res;
+    }
+    int numSquaresRecursive(int &n) {
+        cache.clear();
+        cache.resize(n + 1, -1);
+        return solver(n);
+    }
+    
+    int numSquaresIterative(int &n) {
         vector<int> cache(n + 1, INT_MAX / 2);
         cache[0] = 0;
         for(int curr = 1; curr <= n; curr++) {
@@ -12,5 +27,8 @@ public:
             }
         };
         return cache[n];
+    }
+    int numSquares(int n) {
+       return numSquaresRecursive(n);
     }
 };
