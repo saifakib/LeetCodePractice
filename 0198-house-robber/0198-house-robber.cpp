6644ee7e5f1565b77b81b6a58988efa37a6b17df
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2024-02-16 21:59:36 +0600

    Time: 4 ms (30.76%), Space: 9.2 MB (40.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,15 +3,29 @@
 class Solution {
 public:
     vector<int> cache;
-    int findMaxProfit(int x, vector<int> &nums) {
-        if(x >= nums.size()) return 0;
-        if(cache[x] != -1) return cache[x];
-        int take = nums[x] + findMaxProfit(x+2, nums);
-        int skip = 0 + findMaxProfit(x+1, nums);
-        return cache[x] = max(take, skip);
+    // Recursive way
+    // int findMaxProfit(int x, vector<int> &nums) {
+    //     if(x >= nums.size()) return 0;
+    //     if(cache[x] != -1) return cache[x];
+    //     int take = nums[x] + findMaxProfit(x+2, nums);
+    //     int skip = 0 + findMaxProfit(x+1, nums);
+    //     return cache[x] = max(take, skip);
+    // }
+    int getIndexProfit(int index) {
+        if(index < 0 || index >= cache.size()) return 0; 
+        if(cache[index] != -1) return cache[index];
+        return -1;
     }
     int rob(vector<int>& nums) {
         cache.resize(nums.size(), -1);
-        return findMaxProfit(0, nums);
+        // return findMaxProfit(0, nums);
+        int profit = 0;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            int take = nums[i] + getIndexProfit(i + 2);
+            int skip = getIndexProfit(i + 1);
+            cache[i] = max(take, skip);
+            profit = cache[i];
+        }
+        return profit;
     }
 };
