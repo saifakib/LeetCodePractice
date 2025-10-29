294ec9390211efdea96ceb529b7d6a471294f289
Saif Uddin <45618498+saifakib@users.noreply.github.com>
2025-10-29 10:19:24 +0600

    Time: 2 ms (98.72%), Space: 60.2 MB (81.49%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+//TC: O(|s|) size of s
+//MC: O(1)
+var romanToInt = function(s) {
+    const romanValue = {I:1, V:5, X:10, L:50, C:100, D:500, M:1000};
+    let sum = 0;
+    for(let i = 0; i < s.length; i++) {
+        if(romanValue[s[i]] < romanValue[s[i+1]]) {
+            sum -= romanValue[s[i]]
+        } else {
+            sum +=romanValue[s[i]]
+        }
+    }
+    return sum;
+};
